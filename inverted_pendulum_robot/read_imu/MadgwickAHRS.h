83084d63f27db367b04f6be0b084dc288173ab6a
Jonathan Mathews <peng.inboy@gmail.com>
2021-04-24 19:15:24 -0600

    Filter IMU data and convert to Euler Angles
    Added the Madgwick library to convert IMU data to a quaternion
    representation of the device orientation.

--- inverted_pendulum_robot/read_imu/MadgwickAHRS.h
+++ inverted_pendulum_robot/read_imu/MadgwickAHRS.h
@@ -22,9 +22,16 @@ extern volatile float q0, q1, q2, q3;	// quaternion of sensor frame relative to 
 //---------------------------------------------------------------------------------------------------
 // Function declarations
 
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
 void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
 void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az);
 
+#ifdef __cplusplus
+  }
+#endif
 #endif
 //=====================================================================================================
 // End of file
